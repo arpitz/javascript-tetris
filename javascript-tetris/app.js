18d832cf72d6b79201ebe53777d38e1f4c0e89ce
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-05-16 21:59:41 +0530

    Functionality for start pause button

--- javascript-tetris/app.js
+++ javascript-tetris/app.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector('.grid');
   let squares = Array.from(document.querySelectorAll('.grid div'));
 
-  const ScoreDisplay = document.querySelector('#score');
-  const StartBtn = document.querySelector('#start-button');
+  const scoreDisplay = document.querySelector('#score');
+  const startBtn = document.querySelector('#start-button');
   const width = 10;
   let nextRandom = 0;
+  let timerId;
 
   //The Tetrominoes
   const lTetromino = [
@@ -67,7 +68,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // make the teromino move down every second
-  const timerId = setInterval(moveDown, 1000);
+  // only needed to be called on button click
+  // timerId = setInterval(moveDown, 1000);
 
   // assign functions to keyCodes
   function control(e) {
@@ -170,4 +172,17 @@ document.addEventListener("DOMContentLoaded", () => {
       displaySquares[displayIndex + index].classList.add('tetromino');
     })
   }
+
+  // add functionality to start pause button
+  startBtn.addEventListener('click', () => {
+    if(timerId){
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      nextRandom = Math.floor(Math.random()*theTetrominoes.length);
+      displayShape();
+    }
+  })
 })
